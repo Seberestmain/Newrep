4144fe025367b374ca06377bdd0852c9a441a93a
Alexei Berest <berest.alescha@gmail.com>
2024-03-16 07:41:56 +0300

    Написано 1ое задание

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите значение N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        void rec(int m, int n)
+        {
+        if (m > n)
+        return;
+        if (m % 2 == 0)
+        {
+        Console.Write($"{m}, ");
+        }
+        rec(m+1,n);
+
+        }
+        rec(m,n);
+    
+    }
+}
